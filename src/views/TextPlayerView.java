60be76acf1331e00c2358daa9233941d56381108
William Niemiec <williamniemiec@hotmail.com>
2020-10-01 21:31:06 -0300

    TextArea - Margin

--- src/views/TextPlayerView.java
+++ src/views/TextPlayerView.java
@@ -7,6 +7,7 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -365,6 +366,7 @@ public class TextPlayerView extends JPanel implements View
 		textArea.setWrapStyleWord(true);
 		textArea.setText(text);
 		textArea.setEditable(false);
+		textArea.setMargin(new Insets(10, 10, 10, 10));
 		
 		panel.add(scrollPane, constraints);
 	}
