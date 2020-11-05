c021057d521fadd4847dac7ddbe5b5b702d24675
William Niemiec <williamniemiec@hotmail.com>
2020-11-05 09:20:38 -0300

    Doc

--- src/models/io/input/dialog/InputDialog.java
+++ src/models/io/input/dialog/InputDialog.java
@@ -2,13 +2,48 @@ package models.io.input.dialog;
 
 import java.util.List;
 
+
+/**
+ * Responsible for obtaining an input through a dialog window.
+ * 
+ * @version		1.0.0
+ * @since		1.0.0
+ */
 public abstract class InputDialog
 {
+	//-------------------------------------------------------------------------
+	//		Attributes
+	//-------------------------------------------------------------------------
 	protected boolean wasAsked;
 	protected String title;
 	protected List<String> content;
 	
-	public abstract boolean ask();
+	
+	//-------------------------------------------------------------------------
+	//		Methods
+	//-------------------------------------------------------------------------
+	/**
+	 * Displays dialog input.
+	 * 
+	 * @return		False if the user cancels the operation; True otherwise
+	 */
+	public abstract boolean openDialog();
+	
+	
+	//-------------------------------------------------------------------------
+	//		Getters
+	//-------------------------------------------------------------------------
+	/**
+	 * Gets title of the input provided in {@link #openDialog()}.
+	 * 
+	 * @return		Input content
+	 */
 	public abstract String getTitle();
+	
+	/**
+	 * Gets content of the input provided in {@link #openDialog()}.
+	 * 
+	 * @return		Input content
+	 */
 	public abstract List<String> getContent();
 }
