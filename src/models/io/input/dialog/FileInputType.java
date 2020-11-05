fbb2da2a8c3dbf5aa3eb334fc9fcef6e29278810
William Niemiec <williamniemiec@hotmail.com>
2020-11-05 09:44:08 -0300

    Doc

--- src/models/io/input/dialog/FileInputType.java
+++ src/models/io/input/dialog/FileInputType.java
@@ -2,27 +2,63 @@ package models.io.input.dialog;
 
 import java.awt.FileDialog;
 
+
+/**
+ * Contains file input types available.
+ * 
+ * @version		1.0.0
+ * @since		1.0.0
+ */
 public enum FileInputType
 {
+	//-------------------------------------------------------------------------
+	//		Enumerations
+	//-------------------------------------------------------------------------
 	LOAD("FILE_LOAD_DIALOG_TITLE", FileDialog.LOAD),
 	STORE("FILE_STORE_DIALOG_TITLE", FileDialog.SAVE);
 
-	private int fileDialogType;
-	private String rbKey;
 	
-	private FileInputType(String rbKey, int fileDialogType)
+	//-------------------------------------------------------------------------
+	//		Attributes
+	//-------------------------------------------------------------------------
+	private int fileDialogType;
+	private String langKey;
+	
+	
+	//-------------------------------------------------------------------------
+	//		Constructor
+	//-------------------------------------------------------------------------
+	/**
+	 * @param		langKey Resource bundle key
+	 * @param		fileDialogType {@link FileDialog} type
+	 */
+	private FileInputType(String langKey, int fileDialogType)
 	{
 		this.fileDialogType = fileDialogType;
-		this.rbKey = rbKey;
+		this.langKey = langKey;
 	}
 	
+	
+	//-------------------------------------------------------------------------
+	//		Getters
+	//-------------------------------------------------------------------------
+	/**
+	 * Gets {@link FileDialog} type.
+	 * 
+	 * @return		FileDialog type
+	 */
 	public int getFileDialogType()
 	{
 		return fileDialogType;
 	}
 
-	public String getResourceBundleKey()
+	/**
+	 * Gets resource bundle key.
+	 * 
+	 * @return		Resource bundle key
+	 */
+	public String getLangKey()
 	{
-		return rbKey;
+		return langKey;
 	}
 }
